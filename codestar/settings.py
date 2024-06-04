1d8e7af10cfe1cdf106d1c2c149b82d0806ff13f
Emmelie Kolmodin <e.kolmodin@hotmail.com>
2024-06-04 12:39:01 +0000

    Add models to class Post

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-ko11e-lmsdjango-21t9n9a6k55.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
@@ -93,9 +93,10 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
+    "https://8000-ko11e-lmsdjango-21t9n9a6k55.ws.codeinstitute-ide.net",
     "https://*.codeanyapp.com",
     "https://*.herokuapp.com",
-    "http://*codeinstitute-ide.net"
+    
 ]
 
 # Password validation
